77dc4632e29dcf47e592ba735e8d724450d7ac7b
Nevold <Nevold@tut.by>
2025-05-09 16:25:11 +0300

    feat: start project

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const baseIgnores = ['**/node_modules/**', '**/dist/**', '**/coverage/**', '**/*.config.js'];
+
+export default [
+  {
+    files: ['**/*.ts'],
+    ignores: baseIgnores,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslint,
+      prettier,
+    },
+    rules: {
+      ...tsEslint.configs['recommended'].rules,
+      ...eslintConfigPrettier.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+    },
+  },
+];
